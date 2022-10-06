bc98e5a5992fed26af4091d5d0655fc883d21d28
ExanlesLuaShit <70239152+ExanlesLuashit@users.noreply.github.com>
2022-10-06 07:43:35 +0300

    Update and rename FreeExecutorsBoltsHubV10 to FreeExecutorsBoltsHubV10.lua

--- FreeExecutorsBoltsHubV10.lua
+++ FreeExecutorsBoltsHubV10.lua
@@ -1,3 +1,3 @@
-if game.PlaceId == 286090429 then -- Arsenal
+if game.PlaceId == 292439477 then -- phantom forces
      loadstring(game:HttpGet(('https://raw.githubusercontent.com/NEMOEXEYT/MyScripts/main/BoltsHubV10.lua'),true))()   
 end
